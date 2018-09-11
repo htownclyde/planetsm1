25901ba15f4d71bfea58f82dc1c00bffa7eacc07
Clyde Johnson <htownclyde@gmail.com>
2018-09-10 23:21:39 -0500

    TESTS COMPLETE.

--- src/planetsm1/WeightCalc.java
+++ src/planetsm1/WeightCalc.java
@@ -16,7 +16,7 @@ public class WeightCalc {
         System.out.print("Please enter your name: ");
         int newname = in.nextInt();
         you.setName(newname);
-      EEEEE  System.out.print("Please enter your weight: ");
+        System.out.print("Please enter your weight: ");
         int newweight = in.nextInt();
         you.setWeight(newweight);
         System.out.printf("New number: %s %d\n", you.getName(), you.getWeight());
